ae16f1f7c0401eb3a939409ed38102c591728dda
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-08-01 11:47:30 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OddEven
+{
+    public  class Program
+    {
+        public static void Main(string[] args)
+        {
+            int number = 42;
+            OddEven oddeven = new OddEven();
+            oddeven.ReadNumber(number);
+     
+        }
+    }
+}
