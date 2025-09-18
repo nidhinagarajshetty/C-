2d3594a8edc2dc3a8c84977c06c1d8e81239049c
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-09-19 00:26:13 +0530

    Add files via upload

---
+++ VehicleRentalServices/VehicleRentalServices/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace VehicleRentalServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v = new Vehicle("Accord", "Honda", 300);
+            v.display();
+            Vehicle c = new Car("ZRV", "Honda", 699, 5, "Petrol");
+            c.display();
+        }
+    }
+}
