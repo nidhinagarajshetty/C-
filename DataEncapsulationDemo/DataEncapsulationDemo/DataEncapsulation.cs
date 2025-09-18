5c82a7571115a08e697b9080dda681c952675e5e
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-09-19 00:20:50 +0530

    Add files via upload

---
+++ DataEncapsulationDemo/DataEncapsulationDemo/DataEncapsulation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataEncapsulationDemo
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value > 0 && value < 100)
+                    age = value;
+                else
+                    Console.WriteLine("Invalid age , please enter age between 0 and 100");
+            }
+        }
+    }
+}
---
+++ DataEncapsulationDemo/DataEncapsulationDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace DataEncapsulationDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "bob";
+
+            person.Age = 55;
+
+            Console.WriteLine($"The name is {person.Name} and age is {person.Age}");
+
+        }
+    }
+}
