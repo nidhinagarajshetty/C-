b51849a0dde61b13fb12c9801053ec9b72f3710e
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-09-19 00:25:12 +0530

    Add files via upload

---
+++ PaymentApp/PaymentApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PaymentApp
+{
+    using PaymentApp;
+    using System;
+
+    namespace Store
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Payment creditCardPayment = new CreditCardPayment(2000, DateTime.Now, "1234567890", "abc");
+                creditCardPayment.ProcessPayment();
+
+                Payment upiPayment = new UPIPayment(100, DateTime.Now, "abc.upi", "Union  Bank");
+                upiPayment.ProcessPayment();
+            }
+        }
+    }
+
+}
