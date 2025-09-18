aed9faf44f6a43717c35b75d5c5aa23c75d4340a
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-09-19 00:22:44 +0530

    Add files via upload

---
+++ Dessert/Dessert/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Dessert
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dessert iceCream = new IceCream();
+            Dessert cake = new Cake();
+            Dessert gulabJamun = new GulabJamun();
+
+            Console.WriteLine("=== Ice-Cream Preparation ===");
+            iceCream.MakeDessert();
+
+            Console.WriteLine("=== Cake Preparation ===");
+            cake.MakeDessert();
+
+            Console.WriteLine("=== Gulab Jamun Preparation ===");
+            gulabJamun.MakeDessert();
+        }
+    }
+}
---
+++ Dessert/Dessert/dessert.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dessert
+{
+    abstract class Dessert
+    {
+
+        public void AddSugar()
+        {
+            Console.WriteLine("Step 1: Add Sugar");
+        }
+
+
+        public abstract void Prepare();
+
+
+        public void Serve()
+        {
+            Console.WriteLine("Step 3: Serve to the Customer\n");
+        }
+
+        // Template method that calls all steps in order
+        public void MakeDessert()
+        {
+            AddSugar();
+            Prepare();   // Dessert-specific step
+            Serve();
+        }
+    }
+
+    // Ice-cream dessert
+    class IceCream : Dessert
+    {
+        public override void Prepare()
+        {
+            Console.WriteLine("Step 2: Churn and Freeze the mixture");
+        }
+    }
+
+    // Cake dessert
+    class Cake : Dessert
+    {
+        public override void Prepare()
+        {
+            Console.WriteLine("Step 2: Bake in the oven");
+        }
+    }
+
+    // Gulab Jamun dessert
+    class GulabJamun : Dessert
+    {
+        public override void Prepare()
+        {
+            Console.WriteLine("Step 2: Fry dough balls and soak in syrup");
+        }
+    }
+}
