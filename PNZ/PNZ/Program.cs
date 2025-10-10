d182551cf12be315f116b81de80c08161b2fcf8e
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-10-10 12:25:17 +0530

    Add files via upload

---
+++ PNZ/PNZ/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PNZ
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            pnz n = new pnz(num);
+            n.check();
+        }
+    }
+}
---
+++ PNZ/PNZ/pnz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PNZ
+{
+    public  class pnz
+    {
+        int num;
+        public pnz()
+        {
+            num = 2;
+        }
+        public pnz(int num)
+        {
+            this.num = num;
+        }
+        public void check()
+        {
+            if (num == 0)
+            {
+                Console.WriteLine("Thie number is zero");
+            }
+            else if (num > 0)
+            {
+                Console.WriteLine("The number is positive");
+            }
+            else
+            {
+                Console.WriteLine("The number is negative");
+            }
+        }
+    }
+    
+    }
+
