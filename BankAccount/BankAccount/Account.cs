0d73e73db30dbb30f6414c096139b9a08476f955
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-10-10 12:20:46 +0530

    Add files via upload

---
+++ BankAccount/BankAccount/Account.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    public class Account
+    {
+        public int balance = 0;
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine("Deposited:" + amount);
+        }
+        public void withdraw(int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine("withdraw amount:" + amount);
+            }
+            else
+            {
+                Console.WriteLine("not enough balance!");
+            }
+        }
+        public void ShowBalance()
+        {
+            Console.WriteLine("current balance:" + balance);
+
+        }
+    }
+}
---
+++ BankAccount/BankAccount/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace BankAccount
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Account acc= new Account();
+            acc.Deposit(1000);
+            acc.ShowBalance();
+
+            acc.withdraw(2000); 
+            acc.ShowBalance();
+
+            acc.withdraw(500);
+        }
+    }
+}
