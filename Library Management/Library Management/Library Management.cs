823cee4c31e40d4f92ccf317a8b982e4cfd717df
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-10-10 12:19:10 +0530

    Add files via upload

---
+++ Library Management/Library Management/Library Management.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_Management
+{
+    internal class Book
+    {
+        protected string title;
+        protected string author;
+
+        public Book(string title, string author)
+        {
+            this.title = title;
+            this.author = author;
+        }
+
+        public virtual void DisplayDetails()
+        {
+            Console.WriteLine($"Book Name:{title}");
+            Console.WriteLine($"Book Author:{author}");
+        }
+
+    }
+    class EBook : Book
+    {
+        double filesize;
+        string format;
+
+        public EBook(string title, string author, double filesize, string format) : base(title, author)
+        {
+            this.filesize = filesize;
+            this.format = format;
+        }
+
+        public override void DisplayDetails()
+        {
+            Console.WriteLine($"Book Name:{title}");
+            Console.WriteLine($"Book Author:{author}");
+            Console.WriteLine($"File Size:{filesize}");
+            Console.WriteLine($"Format:{format}");
+
+
+        }
+
+
+    }
+}
---
+++ Library Management/Library Management/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Library_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b = new Book("Diary of A Wimpy Kid", "Jeff Kinney");
+            b.DisplayDetails();
+            Book e = new EBook("Atomic Habits", "James Clear", 100, "ATH");
+            e.DisplayDetails();
+        }
+    }
+}
