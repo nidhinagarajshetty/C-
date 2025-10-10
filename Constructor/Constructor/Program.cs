2827b0aaa37ba4a21d4c2b38842c805612daf5d3
nidhinagarajshetty <nidhishetty035@gmail.com>
2025-10-10 12:24:07 +0530

    Add files via upload

---
+++ Constructor/Constructor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Constructor
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Def1 d = new Def1("John", 30);
+            d.DisplayInfo();
+        }
+    }
+}
---
+++ Constructor/Constructor/constructor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+ 
+    public  class Def1
+    {
+        
+        public string name;
+        public int age;
+
+            public Def1(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+          
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name: {name}, Age: {age}");
+        }
+    }
+}
+
+
